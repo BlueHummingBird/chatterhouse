7ee8b5c93871dd624f21ecf80d6d2c9adb600c21
claire <cdersky@gmail.com>
2015-11-09 13:49:40 -0800

    added a server and Procfile

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+/////////////////////////////////////////////////////////////////
+//       Express Server
+/////////////////////////////////////////////////////////////////
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+var port = process.env.PORT || '3000';
+
+app.use(express.static(__dirname + '/'));
+
+http.listen(port, function() {
+  console.log('listening on port :' + port);
+});
